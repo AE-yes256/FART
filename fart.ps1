a4cb3a8552d1b88b4e369a5b79cd0d14f24d6907
AE-yes256 <35268084+AE-yes256@users.noreply.github.com>
2023-11-14 11:39:03 +0000

    Updates to fart.ps1
    Following Features Added:
    - Made the entire script into a function
    - Added -Selectsub and -All Flags
    - Added Export location to home Dir

--- fart.ps1
+++ fart.ps1
@@ -1,96 +1,179 @@
 <#
 .Synopsis
-A function used to export all NSGs rules, check your Azure flow logs for hits relating to these NSG's in all your Azure Subscriptions and format them in a .csv
+A function used to export all NSGs rules in all your Azure Subscriptions
 
 .DESCRIPTION
 # PowerShell function perform NSG Review
 
 .Notes
-Created   : 11-October-2023
-Updated   : 11-October-2023
-Version   : 1.0
-Author    : NoodleStorm
-
-Replace hashed out valued '###' with your values
-
+Created   : 02-September-2022
+Updated   : 02-September-2022
+Version   : 1
+Author    : Sam Greaves
 Disclaimer: This script is provided "AS IS" with no warranties.
 #>
+Function Get-AzNSG-Review {
+    [cmdletbinding()]
+    Param (
+        [switch]$All, [switch]$SelectSub, [string]$Output
+    )
+    # End of Parameters
 
-$sub = ###############################"
-Set-AzContext -Subscription $sub | Out-Null
-$outputarray = @()
+    Process {
+        Connect-AzAccount -TenantId ##YourTenantID
+        $sub = "###################" ##Your Sub for LAWS
+        Set-AzContext -Subscription $sub | Out-Null
+        $outputarray = @()
 
-$workspaceName = "###############################""
-$workspaceRG = "###############################""
-$WorkspaceID = (Get-AzOperationalInsightsWorkspace -Name $workspaceName -ResourceGroupName $workspaceRG).CustomerID
+        $workspaceName = "###################" ## LAWS Name
+        $workspaceRG = "###################" ## LAWS RG
+        $WorkspaceID = (Get-AzOperationalInsightsWorkspace -Name $workspaceName -ResourceGroupName $workspaceRG).CustomerID
 
-function get-hitcount ($rule, $nsName) {
-    $query = "AzureNetworkAnalytics_CL 
+        function get-hitcount ($rule, $nsName) {
+            $query = "AzureNetworkAnalytics_CL 
     | where TimeGenerated > ago(15d)
     | where SubType_s == `"FlowLog`"
     | where NSGRule_s contains `"$rule`"
     | where NSGList_s endswith `"$nsName`"
     | summarize count()
     "
-    $kqlQuery = Invoke-AzOperationalInsightsQuery -WorkspaceId $WorkspaceID -Query $query
-    $kqlQuery.Results
+            $kqlQuery = Invoke-AzOperationalInsightsQuery -WorkspaceId $WorkspaceID -Query $query
+            $kqlQuery.Results
     
 
-}
-
-$azNsgs = Get-AzNetworkSecurityGroup
-foreach ( $azNsg in $azNsgs ) {
-    $attachedSubnets = $azNsg.Subnets.Id | ForEach-Object { ($_ -split '/')[-1] }
-    $attachedNics = $azNsg.NetworkInterfaces.Id | ForEach-Object { ($_ -split '/')[-1] }
-    # Export custom rules
-    $CustomNSGRules = Get-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $azNsg
-    foreach ( $rule in $CustomNSGRules ) {
-        $CustomRules = [ordered]@{
-            'NSG Name'               = $azNsg.Name | Out-String
-            'NSG Location'           = $azNsg.Location | Out-String
-            'Rule Name'              = $rule.Name | Out-String
-            'Source'                 = $rule.SourceAddressPrefix | Out-String
-            'Source Port Range'      = $rule.SourcePortRange | Out-String
-            'Access'                 = $rule.Access | Out-String
-            'Priority'               = $rule.Priority | Out-String 
-            'Direction'              = $rule.Direction | Out-String
-            'Destination'            = $rule.DestinationAddressPrefix | Out-String
-            'Destination Port Range' = $rule.DestinationPortRange | Out-String
-            'Resource Group Name'    = $azNsg.ResourceGroupName | Out-String
-            'Hit Count'              = get-hitcount $rule.Name $azNsg.Name | Out-String
-            'Attached Subnet(s)'     = $attachedSubnets | Out-String
-            'Attached nic(s)'        = $attachedNics | Out-String
-
-
         }
-        $customrulesobj = new-object -Type PSObject -Property $CustomRules
-        $outputarray += $customrulesobj
-    }
+
+        $counter = 0
+        if ($All) {
+            Clear-Host "All selected to export all NSG's to review all subscriptions you can access"
+            $azSubs = Get-AzSubscription
+            foreach ( $azSub in $azSubs ) {
+                $counter++
+                Write-Progress -Activity "Gathering NSG's for "$azSub.Name"" -CurrentOperation $azSub -PercentComplete (($counter / $azSubs.count) * 100)
+                Set-AzContext -Subscription $azSub | Out-Null
+                $azSubName = $azSub.Name
+                $azNewSubName = $azSubName -replace '(\W)', '_'
+                $azNsgs = Get-AzNetworkSecurityGroup
+                $azNsgs = Get-AzNetworkSecurityGroup
+                foreach ( $azNsg in $azNsgs ) {
+                    $attachedSubnets = $azNsg.Subnets.Id | ForEach-Object { ($_ -split '/')[-1] }
+                    $attachedNics = $azNsg.NetworkInterfaces.Id | ForEach-Object { ($_ -split '/')[-1] }
+                    # Export custom rules
+                    $CustomNSGRules = Get-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $azNsg
+                    foreach ( $rule in $CustomNSGRules ) {
+                        $CustomRules = [ordered]@{
+                            'NSG Name'               = $azNsg.Name | Out-String
+                            'NSG Location'           = $azNsg.Location | Out-String
+                            'Rule Name'              = $rule.Name | Out-String
+                            'Source'                 = $rule.SourceAddressPrefix | Out-String
+                            'Source Port Range'      = $rule.SourcePortRange | Out-String
+                            'Access'                 = $rule.Access | Out-String
+                            'Priority'               = $rule.Priority | Out-String 
+                            'Direction'              = $rule.Direction | Out-String
+                            'Destination'            = $rule.DestinationAddressPrefix | Out-String
+                            'Destination Port Range' = $rule.DestinationPortRange | Out-String
+                            'Resource Group Name'    = $azNsg.ResourceGroupName | Out-String
+                            'Hit Count'              = get-hitcount $rule.Name $azNsg.Name
+                            'Attached Subnet(s)'     = $attachedSubnets | Out-String
+                            'Attached nic(s)'        = $attachedNics | Out-String
+
+                        }
+                        $customrulesobj = new-object -Type PSObject -Property $CustomRules
+                        $outputarray += $customrulesobj
+                    }# EO Foreach
 
 
-    $DefaultNSGRules = Get-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $azNsg -Defaultrules
-    foreach ( $rule in $DefaultNSGRules ) {
-        $CustomRules = [ordered]@{
-            'NSG Name'               = $azNsg.Name | Out-String
-            'NSG Location'           = $azNsg.Location | Out-String
-            'Rule Name'              = $rule.Name | Out-String
-            'Source'                 = $rule.SourceAddressPrefix | Out-String
-            'Source Port Range'      = $rule.SourcePortRange | Out-String
-            'Access'                 = $rule.Access | Out-String
-            'Priority'               = $rule.Priority | Out-String 
-            'Direction'              = $rule.Direction | Out-String
-            'Destination'            = $rule.DestinationAddressPrefix | Out-String
-            'Destination Port Range' = $rule.DestinationPortRange | Out-String
-            'Resource Group Name'    = $azNsg.ResourceGroupName | Out-String
-            'Hit Count'              = get-hitcount $rule.Name $azNsg.Name | Out-String
-            'Attached Subnet(s)'     = $attachedSubnets | Out-String
-            'Attached nic(s)'        = $attachedNics | Out-String
-        }
-        $customrulesobj = new-object -Type PSObject -Property $CustomRules
-        $outputarray += $customrulesobj
-    }
-
-   
-}
-$outputarray
-$outputarray | Export-Csv  -NoTypeInformation -path C:\FART.csv  -Encoding UTF8 -Force
+                    $DefaultNSGRules = Get-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $azNsg -Defaultrules
+                    foreach ( $rule in $DefaultNSGRules ) {
+                        $CustomRules = [ordered]@{
+                            'NSG Name'               = $azNsg.Name | Out-String
+                            'NSG Location'           = $azNsg.Location | Out-String
+                            'Rule Name'              = $rule.Name | Out-String
+                            'Source'                 = $rule.SourceAddressPrefix | Out-String
+                            'Source Port Range'      = $rule.SourcePortRange | Out-String
+                            'Access'                 = $rule.Access | Out-String
+                            'Priority'               = $rule.Priority | Out-String 
+                            'Direction'              = $rule.Direction | Out-String
+                            'Destination'            = $rule.DestinationAddressPrefix | Out-String
+                            'Destination Port Range' = $rule.DestinationPortRange | Out-String
+                            'Resource Group Name'    = $azNsg.ResourceGroupName | Out-String
+                            'Hit Count'              = get-hitcount $rule.Name $azNsg.Name
+                            'Attached Subnet(s)'     = $attachedSubnets | Out-String
+                            'Attached nic(s)'        = $attachedNics | Out-String
+                        }
+                        $customrulesobj = new-object -Type PSObject -Property $CustomRules
+                        $outputarray += $customrulesobj
+                    }# EO Foreach
+                }# EO Foreach
+            }#EO Foreach
+        }#EO If
+        if ($SelectSub) {
+            $azSubs = Get-AzSubscription
+            Clear-Host "-SelectSub selected to export a particular sub's NSG's to. Please make a selection from the below list of subs..."
+            Write-Output ($azSubs | Format-Table | Out-String)
+            $sub = Read-Host "Please enter Target Sub ID"
+            Write-Output "$sub, selected"
+            Set-AzContext -Subscription $sub | Out-Null
+            $Subactual = Get-AzSubscription -SubscriptionId $sub
+            $azSubName = $Subactual.Name
+            $azNewSubName = $azSubName -replace '(\W)', '_'
+            $azNsgs = Get-AzNetworkSecurityGroup   
+            foreach ( $azNsg in $azNsgs ) {
+                $counter++
+                Write-Progress -Activity "Gathering NSG's for "$azSubName"" -CurrentOperation $azNsg -PercentComplete (($counter / $azNsgs.count) * 100)
+                $attachedSubnets = $azNsg.Subnets.Id | ForEach-Object { ($_ -split '/')[-1] }
+                $attachedNics = $azNsg.NetworkInterfaces.Id | ForEach-Object { ($_ -split '/')[-1] }
+                # Export custom rules
+                $CustomNSGRules = Get-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $azNsg
+                foreach ( $rule in $CustomNSGRules ) {
+                    $CustomRules = [ordered]@{
+                        'NSG Name'               = $azNsg.Name | Out-String
+                        'NSG Location'           = $azNsg.Location | Out-String
+                        'Rule Name'              = $rule.Name | Out-String
+                        'Source'                 = $rule.SourceAddressPrefix | Out-String
+                        'Source Port Range'      = $rule.SourcePortRange | Out-String
+                        'Access'                 = $rule.Access | Out-String
+                        'Priority'               = $rule.Priority | Out-String 
+                        'Direction'              = $rule.Direction | Out-String
+                        'Destination'            = $rule.DestinationAddressPrefix | Out-String
+                        'Destination Port Range' = $rule.DestinationPortRange | Out-String
+                        'Resource Group Name'    = $azNsg.ResourceGroupName | Out-String
+                        'Hit Count'              = get-hitcount $rule.Name $azNsg.Name
+                        'Attached Subnet(s)'     = $attachedSubnets | Out-String
+                        'Attached nic(s)'        = $attachedNics | Out-String
+                
+                
+                    }
+                    $customrulesobj = new-object -Type PSObject -Property $CustomRules
+                    $outputarray += $customrulesobj
+                }# EO Foreach
+                
+                
+                $DefaultNSGRules = Get-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $azNsg -Defaultrules
+                foreach ( $rule in $DefaultNSGRules ) {
+                    $CustomRules = [ordered]@{
+                        'NSG Name'               = $azNsg.Name | Out-String
+                        'NSG Location'           = $azNsg.Location | Out-String
+                        'Rule Name'              = $rule.Name | Out-String
+                        'Source'                 = $rule.SourceAddressPrefix | Out-String
+                        'Source Port Range'      = $rule.SourcePortRange | Out-String
+                        'Access'                 = $rule.Access | Out-String
+                        'Priority'               = $rule.Priority | Out-String 
+                        'Direction'              = $rule.Direction | Out-String
+                        'Destination'            = $rule.DestinationAddressPrefix | Out-String
+                        'Destination Port Range' = $rule.DestinationPortRange | Out-String
+                        'Resource Group Name'    = $azNsg.ResourceGroupName | Out-String
+                        'Hit Count'              = get-hitcount $rule.Name $azNsg.Name
+                        'Attached Subnet(s)'     = $attachedSubnets | Out-String
+                        'Attached nic(s)'        = $attachedNics | Out-String
+                    }
+                    $customrulesobj = new-object -Type PSObject -Property $CustomRules
+                    $outputarray += $customrulesobj
+                }# EO Foreach
+                
+                   
+            }
+        } #End of IF
+        $outputarray | Export-Csv  -Path "$($home)\$azNewSubName-nsg-rules.csv" -NoTypeInformation -Append -Encoding UTF8 -Force
+    }#EO Process
+}#EO Function
